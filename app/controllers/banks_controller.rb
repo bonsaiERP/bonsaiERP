61eb851a7b1b4ed02683c1053f2e7153294ff24c
boriscy <boriscyber@gmail.com>
2011-02-08 13:47:41 -0400

    Working javascript for payments

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,4 +1,5 @@
 class BanksController < ApplicationController
+  before_filter :find_bank, :only => [:show, :edit, :update, :destroy]
   # GET /banks
   # GET /banks.xml
   def index
@@ -13,8 +14,6 @@ class BanksController < ApplicationController
   # GET /banks/1
   # GET /banks/1.xml
   def show
-    @bank = Bank.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @bank }
@@ -34,7 +33,6 @@ class BanksController < ApplicationController
 
   # GET /banks/1/edit
   def edit
-    @bank = Bank.find(params[:id])
   end
 
   # POST /banks
@@ -44,7 +42,7 @@ class BanksController < ApplicationController
 
     respond_to do |format|
       if @bank.save
-        format.html { redirect_to(@bank, :notice => 'Bank was successfully created.') }
+        format.html { redirect_to(@bank, :notice => 'El banco fue creado.') }
         format.xml  { render :xml => @bank, :status => :created, :location => @bank }
       else
         format.html { render :action => "new" }
@@ -56,11 +54,9 @@ class BanksController < ApplicationController
   # PUT /banks/1
   # PUT /banks/1.xml
   def update
-    @bank = Bank.find(params[:id])
-
     respond_to do |format|
       if @bank.update_attributes(params[:bank])
-        format.html { redirect_to(@bank, :notice => 'Bank was successfully updated.') }
+        format.html { redirect_to(@bank, :notice => 'Banco actualizado.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -72,9 +68,12 @@ class BanksController < ApplicationController
   # DELETE /banks/1
   # DELETE /banks/1.xml
   def destroy
-    @bank = Bank.find(params[:id])
     @bank.destroy
-
     respond_ajax @bank
   end
+
+  protected
+    def find_bank
+      @bank = Bank.find(params[:id])
+    end
 end
