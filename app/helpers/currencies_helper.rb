e11ee70fe9016c68cc1beb349836984b71a18cd8
boriscy <boriscyber@gmail.com>
2011-03-10 14:48:36 -0400

    Removed barista

--- app/helpers/currencies_helper.rb
+++ app/helpers/currencies_helper.rb
@@ -1,7 +1,3 @@
 module CurrenciesHelper
-  def with_currency(klass, amount = :amount, options = {})
-    options = {:precision => 2}.merge(options)
-    "#{ klass.currency_symbol } #{number_to_currency klass.send(amount), options}"
-  end
 
 end
