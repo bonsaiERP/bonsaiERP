d1ecb585d9dec3de263307dd18634fb08a61a831
Boris Barroso <boriscyber@gmail.com>
2011-04-28 11:44:51 -0400

    Sotore creation when selecting new store for operations

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -5,7 +5,7 @@ class StoresController < ApplicationController
   # GET /stores
   # GET /stores.xml
   def index
-    @stores = Store.all
+    @stores = Store.org
 
     respond_to do |format|
       format.html # index.html.erb
@@ -27,7 +27,7 @@ class StoresController < ApplicationController
   # GET /stores/new
   # GET /stores/new.xml
   def new
-    @store = Store.new
+    @store = Store.new(:active => true)
 
     respond_to do |format|
       format.html # new.html.erb
@@ -45,14 +45,10 @@ class StoresController < ApplicationController
   def create
     @store = Store.new(params[:store])
 
-    respond_to do |format|
-      if @store.save
-        format.html { redirect_to(@store, :notice => 'El almacen fue correctamente creado.') }
-        format.xml  { render :xml => @store, :status => :created, :location => @store }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @store.errors, :status => :unprocessable_entity }
-      end
+    if @store.save
+      redirect_ajax(@store, :notice => 'El almacen fue correctamente creado.')
+    else
+      render :action => "new"
     end
   end
 
