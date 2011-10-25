8ee9527ad619537f663df1ff954519f9654f33b5
Boris Barroso <boriscyber@gmail.com>
2011-10-25 17:26:52 -0400

    Presenter for user

---
+++ app/presenters/user_presenter.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+# author: Boris Barroso
+# email: boriscyber@gmail.com
+class UserPresenter < BasePresenter
+  presents :user
+
+  def edit_link
+    if h.session[:user][:rol] === "admin"
+      h.link_to "Editar datos", edit_user_path(@user), :class => 'edit'
+    end
+  end
+
+  def password_link
+    if h.session[:user_id] === user.id
+      h.link_to "Cambiar contraseña", password_users_path, :class => 'key'
+    end
+  end
+end
