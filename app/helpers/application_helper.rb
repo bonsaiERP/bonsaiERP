33c4891484e86ee8112b39795b7af7f5741fe181
Boris Barroso <boriscyber@gmail.com>
2012-10-27 03:15:02 -0400

    Improving presentation fo currencies

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -254,4 +254,17 @@ module ApplicationHelper
   def currency_code_label
     "<span class='label label-inverse' title='#{currency_name}'>#{currency_code}</span>"
   end
+  alias :cur_clabel :currency_code_label
+
+  def true_false(val)
+    if val
+      'bicon-tick'
+    else
+      'bicon-cross'
+    end
+  end
+
+  def show_amount(amount, cur_id=currency_id)
+    "#{ ntc(amount) } #{cur_clabel}"
+  end
 end
