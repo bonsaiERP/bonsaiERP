1f8330f0e6028b8ba546bd36e6443613b2d7b99c
Boris Barroso <boriscyber@gmail.com>
2013-01-21 14:30:02 -0400

    Removed unused code

--- app/models/personal_comment.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-# author: Boris Barroso
-# email: boriscyber@gmail.com
-class PersonalComment < ActiveRecord::Base
-
-  belongs_to :account_ledger
-
-  validates :comment, :presence => true, :length => {:minimum => 5}
-end
