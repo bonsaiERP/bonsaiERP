548c9edf726588a225af90cc79a80eb2690c6fda
Boris Barroso <boriscyber@gmail.com>
2013-03-15 12:20:42 -0400

    Fixed with allow_nil

--- app/models/organisation_session.rb
+++ app/models/organisation_session.rb
@@ -5,7 +5,7 @@
 class OrganisationSession
   class << self
     attr_reader :organisation
-    delegate :id, :name, :currency, :tenant, :emamil, :address, to: :organisation
+    delegate :id, :name, :currency, :tenant, :emamil, :address, to: :organisation, allow_nil: true
 
 
     # Stores using de application_controller the current_user for devise
