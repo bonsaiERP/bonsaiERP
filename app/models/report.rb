1a269d92708656db0ff40297396b4739bb3bf57d
Boris Barroso <boriscyber@gmail.com>
2013-04-05 14:32:12 -0400

    Improve ranges

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,15 +1,11 @@
 # encoding: utf-8
 class Report
   def expenses_by_item(drange = DateRange.default)
-    ids = Expense.active.where("date BETWEEN ? AND ?", drange.sdate, drange.edate).map(&:id)
-
-    conn.select_rows(sum_transaction_details_sql(ids)).map {|v| DetReport.new(*v)}
+    conn.select_rows(sum_transaction_details_sql('Expense', drange)).map {|v| DetReport.new(*v)}
   end
 
   def incomes_by_item(drange = DateRange.default)
-    ids = Income.active.where("date BETWEEN ? AND ?", drange.sdate, drange.edate).map(&:id)
-
-    conn.select_rows(sum_transaction_details_sql(ids)).map {|v| DetReport.new(*v)}
+    conn.select_rows(sum_transaction_details_sql('Income', drange)).map {|v| DetReport.new(*v)}
   end
 
 private
@@ -17,12 +13,12 @@ private
     ActiveRecord::Base.connection
   end
 
-  def sum_transaction_details_sql(ids)
+  def sum_transaction_details_sql(type, drange)
     <<-SQL
       SELECT i.id, i.name, SUM(d.price * d.quantity * a.exchange_rate) AS total
       FROM transaction_details d JOIN items i ON (i.id = d.item_id)
       JOIN accounts a ON (a.id = d.account_id)
-      WHERE d.account_id in (#{ids.join(", ")})
+      WHERE a.type = '#{type}' AND a.date BETWEEN '#{drange.sdate}' AND '#{drange.edate}'
       GROUP BY (i.id)
       ORDER BY total DESC
       LIMIT 10
@@ -30,10 +26,23 @@ private
   end
 end
 
-class DateRange < Struct.new(:sdate, :edate)
+class DateRange < Struct.new(:start_date, :end_date)
+  alias :sdate :start_date
+  alias :edate :end_date
+
   def self.default
     d = Date.today
     new(d - 30.days, d)
+  end
+
+  def self.last(days = 30)
+    d = Date.today
+    new(d - days.days, d)
+  end
+
+  def self.range(s, e)
+    s, e = (s.is_a?(String) ? Date.parse(s) : s), (e.is_a?(String) ? Date.parse(e) : e)
+    new(s, e)
   end
 end
 
