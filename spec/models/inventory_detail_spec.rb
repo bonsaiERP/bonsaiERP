8570e9f76e3383b14337eeabdaa3d409171ec834
Boris Barroso <boriscyber@gmail.com>
2013-06-12 18:20:08 -0400

    Remove unwanted spec data

--- spec/models/inventory_detail_spec.rb
+++ spec/models/inventory_detail_spec.rb
@@ -4,8 +4,6 @@ describe InventoryDetail do
   it { should belong_to(:inventory) }
   it { should belong_to(:item) }
 
-  #it { should validate_presence_of(:inventory_operation) }
-  #it { should validate_presence_of(:inventory_operation_id) }
   it { should validate_presence_of(:item) }
   it { should validate_presence_of(:item_id) }
 
