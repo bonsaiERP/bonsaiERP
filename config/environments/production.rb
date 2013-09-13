3b49013dc81a3c5397b7d8e66717d2210f8534e2
Boris Barroso <boriscyber@gmail.com>
2013-09-13 13:40:35 -0400

    Compress assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,11 +12,13 @@ Bonsaierp::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  #config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :sass
   # Generate digests for assets URLs
   config.assets.digest = true
 
