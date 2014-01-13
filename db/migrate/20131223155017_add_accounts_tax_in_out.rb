a4b98979cffce63a7bf691600db940d46f4a7ae5
Boris Barroso <boriscyber@gmail.com>
2014-01-13 14:45:02 -0400

    Add index after adding column

--- db/migrate/20131223155017_add_accounts_tax_in_out.rb
+++ db/migrate/20131223155017_add_accounts_tax_in_out.rb
@@ -3,8 +3,8 @@ class AddAccountsTaxInOut < ActiveRecord::Migration
     PgTools.with_schemas except: 'common' do
       change_table :accounts do |t|
         t.boolean :tax_in_out, default: false
-        t.index :tax_in_out
       end
+      add_index :accounts, :tax_id
     end
   end
 end
