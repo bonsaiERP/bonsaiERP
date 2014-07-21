6427ca51e9cae1bff7730ee3ec65ee0fc9a6f1df
Boris Barroso <boriscyber@gmail.com>
2014-07-21 10:25:11 -0400

    Passing mailers tests

--- spec/mailers/reset_password_mailer_spec.rb
+++ spec/mailers/reset_password_mailer_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 describe ResetPasswordMailer do
   let(:user) {
     User.new(:email => "demo@example.com") {|u|
-      u.reset_password_token = SecureRandom.base64(12)
+      u.reset_password_token = SecureRandom.urlsafe_base64(12)
       u.id = 1
     }
   }
@@ -18,7 +18,8 @@ describe ResetPasswordMailer do
     mail.subject.should == I18n.t("email.reset_password.subject", app_name: APP_NAME)
     mail.to.should == [user.email]
 
-    url = "#{HTTP_PROTOCOL}://#{DOMAIN}/reset_passwords/#{user.reset_password_token}/edit"
+    url = "#{HTTP_PROTOCOL}://app.#{DOMAIN}/reset_passwords/#{user.reset_password_token}/edit"
+
     mail.body.should have_selector('h1', text: "Recuperación de contraseña en #{APP_NAME}")
     mail.body.should have_selector("a[href='#{url}']")
   end
