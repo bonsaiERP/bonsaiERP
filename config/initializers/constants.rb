5fcd86a6edf2d8c5f09d90bb181bf6266b2cdf62
Boris Barroso <boriscyber@gmail.com>
2014-07-21 15:27:43 -0400

    Fixed constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,6 +2,7 @@ PASSWORD_LENGTH = 8
 DOMAIN = Rails.application.secrets.domain
 ENV['DOMAIN'] = DOMAIN
 HTTP_PROTOCOL = Rails.application.secrets.http_protocol
+ENV['MANDRILL_API_KEY'] = Rails.application.secrets.mandrill_api_key
 DEV_DOMAIN = 'localhost.bom'
 APP_NAME = 'bonsaiERP'
 ALLOW_REGISTRATIONS = true
