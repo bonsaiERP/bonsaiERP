3412156afff58f092aa610fe09703ab23e4529f8
Boris Barroso <boriscyber@gmail.com>
2015-05-19 10:04:03 -0400

    Loans LedgerIn with exchange_rate

--- app/models/loans/ledger_in_form.rb
+++ app/models/loans/ledger_in_form.rb
@@ -1,15 +1,18 @@
-class LoanLedgerInForm < BaseForm
+class Loans::LedgerInForm < BaseForm
   attribute :loan_id, Integer
   attribute :account_to_id, Integer
   attribute :amount, BigDecimal
   attribute :reference, String
   attribute :date, Date
-  attribute :exchange_rate, Decimal
+  attribute :exchange_rate, Decimal, default: 1
   attribute :verification, Boolean, default: false
 
+  # Validations
   validates :account_to, presence: true
   validates :amount, numericality: {greater_than: 0}
 
+  # Delegates
+  delegate :exchange_rate, to: :currency_exchange, prefix: 'cur'
 
   def loan
     @_loan ||= Loan.find(loan_id)
@@ -19,7 +22,7 @@ class LoanLedgerInForm < BaseForm
     return false  unless valid?
 
     loan.class.transaction do
-      loan.amount += amount
+      loan.amount += amount * cur_exchange_rate
       ledger_in.save
 
       loan.save && ledger_in.save_ledger
@@ -31,8 +34,11 @@ class LoanLedgerInForm < BaseForm
       account_id: loan.id,
       account_to_id: account_to_id,
       amount: get_amount,
+      exchange_rate: cur_exchange_rate,
       reference: reference,
+      operation: get_operation,
       date: date,
+      status: get_status,
       contact_id: loan.contact_id
     )
   end
@@ -49,6 +55,24 @@ class LoanLedgerInForm < BaseForm
       else
         amount
       end
+    end
+
+    def get_operation
+      if loan.is_a?(Loans::Give)
+        'lgcre'
+      else
+        'lrcre'
+      end
+    end
+
+    def get_status
+      verification == true ? 'pendent' : 'approved'
+    end
+
+    def currency_exchange
+      @currency_exchange ||= CurrencyExchange.new(
+        account: loan, account_to: account_to, exchange_rate: exchange_rate
+      )
     end
 
 end
