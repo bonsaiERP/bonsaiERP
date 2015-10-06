af48a385587fd127366a531d717ce0182c30c5c3
Boris Barroso <boriscyber@gmail.com>
2015-10-06 10:06:39 -0400

    Fixing currencies

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
-ruby '2.2.2'
-gem 'rails', '4.2.3'
+ruby '2.2.3'
+gem 'rails', '4.2.4'
 
 # Assets
 gem 'sass-rails', '~> 5.0.1'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.2.1'
+lock '3.4.0'
 
 set :application, 'bonsaierp'
 set :repo_url, 'git@bitbucket.org:boriscyber/bonsaierp.git'
